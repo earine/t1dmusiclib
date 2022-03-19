66a952f1b85bc818fc0a3624f96b78eb9b0a730f
Marina Lunts <mlunts@pixsystem.com>
2022-03-19 11:22:34 +0100

    Initial Commit

---
+++ musiclib/musiclibApp.swift
@@ -0,0 +1,17 @@
+//
+//  musiclibApp.swift
+//  musiclib
+//
+//  Created by mlunts on 19.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct musiclibApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
